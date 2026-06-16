4e964c709869dcc0ae41f82f921d925ae6a3b98c
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-06-16 15:45:39 -0600

    Modify bloqueNavegador function

--- build/js/app.js
+++ build/js/app.js
@@ -2,21 +2,42 @@ document.addEventListener('DOMContentLoaded', function () {
     bloquearNavegador();
     loopVideos();
 })
-
 function bloquearNavegador() {
-    const header = document.querySelector('.header')
-    const informacion = document.querySelector('.informacion-texto')
+    const header = document.querySelector('.header');
+    const informacion = document.querySelector('.informacion');
 
     document.addEventListener('scroll', function () {
-        if (informacion.getBoundingClientRect().bottom < 1) {
+        if (informacion.getBoundingClientRect().top < 800) {
             header.classList.add('fixed');
         } else {
             header.classList.remove('fixed');
-
         }
-    })
+    });
+}
 
+function loopVideos() {
+    let numeroVideo = 1;
+    const limiteTiempo = 4;
 
+    const reproductor = document.createElement("VIDEO");
+    const clasevideos = document.querySelector('.video');
+    reproductor.src = `src/video/video${numeroVideo}.mp4`;
+    reproductor.muted = true;
+    reproductor.autoplay = true;
+
+    clasevideos.appendChild(reproductor);
+
+    reproductor.addEventListener('ended', function () {
+        if (numeroVideo == limiteTiempo) {
+            numeroVideo = 1;
+        } else {
+            numeroVideo = numeroVideo + 1;
+        }
+        
+        // Cambiamos el video y le damos PLAY de inmediato para que no se pause
+        reproductor.src = `src/video/video${numeroVideo}.mp4`;
+        reproductor.play(); 
+    });
 }
 
 function loopVideos() {
