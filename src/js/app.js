cd5717921ff64cd50283cdd18514d88a1341de02
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-06-16 16:57:44 -0600

    IMPLEMENATACION EN SRC NO EN BULD DE APP

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+    bloquearNavegador();
     loopVideos();
 })
 
+function bloquearNavegador() {
+    const header = document.querySelector('.header');
+    const informacion = document.querySelector('.informacion');
+
+    document.addEventListener('scroll', function () {
+        if (informacion.getBoundingClientRect().top < 1000) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    })
+
+
+}
+
 function loopVideos() {
     let numeroVideo = 1;
-    const limiteTiempo = 5;
+    const limiteTiempo = 4;
 
     const reproductor = document.createElement("VIDEO");
     const clasevideos = document.querySelector('.video')
@@ -16,7 +32,13 @@ function loopVideos() {
 
     reproductor.addEventListener('ended', function () {
 
-        numeroVideo = numeroVideo + 1;
+        if (numeroVideo != limiteTiempo) {
+            numeroVideo = numeroVideo + 1;
+            reproductor.src = `src/video/video${numeroVideo}.mp4`;
+        } else {
+            numeroVideo = 1;
+            reproductor.src = `src/video/video${numeroVideo}.mp4`;
+        }
 
     })
 
